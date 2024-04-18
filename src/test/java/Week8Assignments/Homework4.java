2f0fff4e8111a95376e3a425f34d3e0077df471b
EnasAlkhamisi <Alkhamisienas@gmail.com>
2024-04-18 20:06:00 +0300

    ..

--- src/test/java/Week8Assignments/Homework4.java
+++ src/test/java/Week8Assignments/Homework4.java
@@ -28,16 +28,25 @@ import static org.hamcrest.Matchers.equalTo;
 public class Homework4 {
     @Test
     public void test() {
+
+//        https://reqres.in/api/users/2
+//        User send GET Request to the URL
         Response response = RestAssured.get("https://reqres.in/api/users/2");
         response.prettyPrint();
         // Assertions
         response
                 .then()
+//                       HTTP Status Code should be 200
                 .statusCode(200)
+//                       Response format should be "application/json"
                 .contentType(ContentType.JSON)
+//                       "email" is "janet.weaver@reqres.in",
                 .body("data.email", equalTo("janet.weaver@reqres.in"),
+//                        "first_name" is "Janet"
                         "data.first_name", equalTo("Janet"),
+//                        "last_name" is "Weaver"
                         "data.last_name", equalTo("Weaver"),
+//                        "text" is "To keep ReqRes free, contributions towards server costs are appreciated!"
                         "support.text", equalTo("To keep ReqRes free, contributions towards server costs are appreciated!"));
     }
 }
