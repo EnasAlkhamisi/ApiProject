15ded36ab97a1c9da2394685734aba9f4ca5dab7
EnasAlkhamisi <Alkhamisienas@gmail.com>
2024-05-02 13:56:11 +0300

    ..

--- src/test/java/pojos/PetPojo.java
+++ src/test/java/pojos/PetPojo.java
@@ -1,85 +1,73 @@
 package pojos;
 
-import java.util.ArrayList;
+import java.util.List;
 
-public class PetPojo {
-    private int id;
-    private petcategoryPojo category ;
+public class PetPojo{
+    private List<String> photoUrls;
     private String name;
-    private ArrayList<String> photoUrls;
-    private ArrayList<pettagPojo> tags;
+    private Integer id;
+    private Category category;
+    private List<TagsItem> tags;
     private String status;
 
-    public PetPojo(){
-
+    public void setId(Integer id) {
+        this.id = id;
     }
 
-    public PetPojo(int id, petcategoryPojo category, String name, ArrayList<String> photoUrls, ArrayList<pettagPojo> tags, String status) {
-        this.id = id;
-        this.category = category;
-        this.name = name;
+    public void setPhotoUrls(List<String> photoUrls) {
         this.photoUrls = photoUrls;
-        this.tags = tags;
-        this.status = status;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public petcategoryPojo getCategory() {
-        return category;
-    }
-
-    public void setCategory(petcategoryPojo category) {
-        this.category = category;
-    }
-
-    public String getName() {
-        return name;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public ArrayList<String> getPhotoUrls() {
-        return photoUrls;
+    public void setCategory(Category category) {
+        this.category = category;
     }
 
-    public void setPhotoUrls(ArrayList<String> photoUrls) {
-        this.photoUrls = photoUrls;
-    }
-
-    public ArrayList<pettagPojo> getTags() {
-        return tags;
-    }
-
-    public void setTags(ArrayList<pettagPojo> tags) {
+    public void setTags(List<TagsItem> tags) {
         this.tags = tags;
-    }
-
-    public String getStatus() {
-        return status;
     }
 
     public void setStatus(String status) {
         this.status = status;
     }
 
+    public List<String> getPhotoUrls(){
+        return photoUrls;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public Integer getId(){
+        return id;
+    }
+
+    public Category getCategory(){
+        return category;
+    }
+
+    public List<TagsItem> getTags(){
+        return tags;
+    }
+
+    public String getStatus(){
+        return status;
+    }
+
     @Override
-    public String toString() {
-        return "petPojo{" +
-                "id=" + id +
-                ", category=" + category +
-                ", name='" + name + '\'' +
-                ", photoUrls=" + photoUrls +
-                ", tags=" + tags +
-                ", status='" + status + '\'' +
-                '}';
+    public String toString(){
+        return
+                "PetPojo{" +
+                        "photoUrls = '" + photoUrls + '\'' +
+                        ",name = '" + name + '\'' +
+                        ",id = '" + id + '\'' +
+                        ",category = '" + category + '\'' +
+                        ",tags = '" + tags + '\'' +
+                        ",status = '" + status + '\'' +
+                        "}";
     }
 }
